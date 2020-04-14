85451cb9ab5eafae7185d704e3b2480d7b498a0d
Diego Felix de Almeida <diegobill@gmail.com>
2020-04-14 11:02:05 -0300

    bugfix feature sequential queue, avoid no available tasks

--- background_task/tasks.py
+++ background_task/tasks.py
@@ -243,7 +243,7 @@ class DBTaskRunner(object):
     def get_task_to_run(self, tasks, queue=None):
         try:
             available_tasks = [task for task in Task.objects.find_available(queue)
-                               if task.task_name in tasks._tasks][:5]
+                               if task.task_name in tasks._tasks]
             for task in available_tasks:
                 if task.sequential_queue:
                     first_task = Task.objects.filter(queue=task.queue).order_by('pk')[0]
